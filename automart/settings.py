87ea9b3b07ab2ecc6617035aeddb5476cb77acd5
Aarush Pal <aarushpal2@gmail.com>
2023-04-29 00:28:07 +0530

    added contacts app and finalized project

--- automart/settings.py
+++ automart/settings.py
@@ -27,12 +27,16 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+LOGIN_REDIRECT_URL = 'dashboard'
+
 
 # Application definition
 
 INSTALLED_APPS = [
+'contacts.apps.ContactsConfig',
 'cars.apps.CarsConfig',
 'pages.apps.PagesConfig',
+'accounts.apps.AccountsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -41,6 +45,14 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'ckeditor',
     'django.contrib.humanize',
+    'django.contrib.sites',
+    'allauth',
+    'allauth.account',
+    'allauth.socialaccount',
+
+    #Providers
+    'allauth.socialaccount.providers.facebook',
+    'allauth.socialaccount.providers.google',
 
 ]
 
@@ -131,6 +143,25 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'automart/static'),
 ]
 
+
+
 #media settings
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
+
+
+#messages
+from django.contrib.messages import constants as messages
+MESSAGE_TAGS = {
+    messages.ERROR: 'danger',
+}
+
+SITE_ID = 1
+
+
+# Sending email
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587 #for gmail
+EMAIL_HOST_USER = '********'
+EMAIL_HOST_PASSWORD = '********'
+EMAIL_USE_TLS = True
